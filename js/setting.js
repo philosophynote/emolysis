ddc8a7385ce334df5a7fd218910fe94d8e967cc8
daisky <daisky.shadow@gmail.com>
2021-12-12 13:17:08 +0900

    doctor側のリアルタイムの受信処理を設定

---
+++ js/setting.js
@@ -0,0 +1,29 @@
+// 各種APIKEY
+
+// 音声 ----------------------------------------------------------------------
+window.__VOICE_KEY = "FiykWR4BzYF8C2reMmzmpT4nrH2eVkLM8QL18lPMl3g"
+window.__VOICE_URL = "https://api.webempath.net/v2/analyzeWav"
+//----------------------------------------------------------------------------
+
+// 文字起こし ----------------------------------------------------------------
+window.__SPEACH_TO_TEXT_API_KEY = '732A033BF83405816FD9773D23B112F34A50B0FF4B51D40B2EC97AABCC343B66913C8B2D'
+window.__SPEACH_TO_TEXT_URL = "https://acp-api.amivoice.com/issue_service_authorization";
+window.__SPEACH_TO_TEXT_USER_ID = 'adverdest'
+window.__SPEACH_TO_TEXT_USER_PW = '7wRst2Ijh6oS9y04'
+//----------------------------------------------------------------------------
+
+// 文章感情分析 --------------------------------------------------------------
+window.__TEXT_KEY = "aa1d62d5eae745b19f0965e548d6649a"
+window.__TEXT_URL = "https://emolysis.cognitiveservices.azure.com/text/analytics/v3.2-preview.1/sentiment?opinionMining=true"
+//----------------------------------------------------------------------------
+
+// Firebase
+window.__FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCHmqE7vmJLD3LdiDeIqEcpxSDl9WzvM8o",
+    authDomain: "emolysis-4b500.firebaseapp.com",
+    databaseURL: "https://emolysis-4b500-default-rtdb.firebaseio.com",
+    projectId: "emolysis-4b500",
+    storageBucket: "emolysis-4b500.appspot.com",
+    messagingSenderId: "511170157599",
+    appId: "1:511170157599:web:df40afc824f8fc1fbf092a"
+};
